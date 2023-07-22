0d218b9775f98e2911c9193864e850fa44c0a8a7
Jensan Allred <jensanmadison@Jensans-MacBook-Air.local>
2023-07-21 21:34:29 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+ 
+  return (
+    <div className="">
+
+      <h1>
+        Eco-Friendly Fashion Aggregator Webapp
+      </h1>
+ 
+    </div>
+  );
+}
+
+export default App;
